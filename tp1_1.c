301414f742be1c0e60ca484dbeb4e148c876792a
Diana Park <dianapark411@gmail.com>
2022-03-21 12:19:42 -0300

    primer commit de tp1_1

--- tp1_1.c
+++ tp1_1.c
@@ -4,4 +4,18 @@ int main(){
 
     printf("Hola mundo \n");
 
+    int *punt;
+    int num;
+
+    printf("Ingrese un numero: \n");
+    scanf("%i", &num);
+
+    punt = &num;
+
+    printf("El contenido del puntero es: %i \n", punt);
+    printf("Lo apuntado por el puntero: %i \n", *punt);
+    printf("La direccion de memoria de la variable: %i \n", &num);
+    printf("La direccion de memoria del puntero: %i \n", &punt);
+    printf("El tamanio de memoria utilizado por la variable: %i \n", sizeof(num));
+
 }
