371c1ffd3d6e727b31d08b5e414bcae695985ae5
Diana Park <dianapark411@gmail.com>
2022-03-23 01:07:25 -0300

    Segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main(){
+
     printf("Hola mundo\n");
+
+    int num, *punt; 
+    printf("Ingrese un numero: ");
+    scanf("%i", &num);
+
+    punt = &num;
+
+    printf("\nEl contenido del puntero es: %i", punt);
+    printf("\nEl contenido apuntado por el puntero es: %i", *punt);
+    // printf("\nLa direccion de memoria almacenada por el puntero es: %i", punt);
+    printf("\nLa direccion de memoria de la variable es: %i", &num);
+    printf("\nLa direccion de memoria del puntero es: %i", &punt);
+    printf("\nEl tamanio de la memoria utilizada por la variable es: %i", sizeof(num));
+
+    return 0;
 }
